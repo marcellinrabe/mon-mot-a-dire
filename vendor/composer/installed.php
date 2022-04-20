54c6c5be9531b618545ea0b82c1d5f5996a1cfca
marcellinrabe <marcellinp20.aps1a@gmail.com>
2022-04-20 06:23:48 +0300

    installation bladeOne

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '39d4c311ec15f5b13cc7b810b0fc5dec8ec572a9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '39d4c311ec15f5b13cc7b810b0fc5dec8ec572a9',
+            'dev_requirement' => false,
+        ),
+        'eftec/bladeone' => array(
+            'pretty_version' => '4.5.3',
+            'version' => '4.5.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eftec/bladeone',
+            'aliases' => array(),
+            'reference' => 'b8eb8be9952cd0113469e2ec1e406e075ca433ad',
+            'dev_requirement' => false,
+        ),
+    ),
+);
